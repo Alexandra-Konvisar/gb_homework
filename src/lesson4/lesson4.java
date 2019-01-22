8c7bee399c36726c147faeb35d53e23da67cd5d1
4dimarik <4dimarik@gmail.com>
2019-01-22 13:36:12 +0300

    Задание 2 3

--- src/lesson4/lesson4.java
+++ src/lesson4/lesson4.java
@@ -1,15 +1,9 @@
-package lesson4;
-
 
 import java.util.Random;
 import java.util.Scanner;
 public class lesson4 {
     public static int SIZE = 3;
     public static int DOTS_TO_WIN = 3;
-    public static int DOTS_PRE_WIN = 2;
-    public static boolean WARNING = false;
-    public static int aiX;
-    public static int aiY;
     public static final char DOT_EMPTY = '•';
     public static final char DOT_X = 'X';
     public static final char DOT_O = 'O';
@@ -30,8 +24,7 @@ public class lesson4 {
                 System.out.println("Ничья");
                 break;
             }
-            if(WARNING) {aiThinkTurn(aiX,aiY);} else {aiTurn();}
-
+            aiTurn();
             printMap();
             if (checkWin(DOT_O)) {
                 System.out.println("Победил Искуственный Интеллект");
@@ -44,87 +37,54 @@ public class lesson4 {
         }
         System.out.println("Игра закончена");
     }
-
-    private static void aiThinkX(int x) {
-        for (int y = 0; y < SIZE; y++) {
-            if (map[x][y] == DOT_EMPTY ) {
-                aiX = x;
-                aiY = y;
-            }
-        }
-    }
-
     public static boolean checkWin(char symb) {
         // Переменная для подсчета идущих подряд символов в ряду
-        int preWin = 0;
-        /*
-            Проверка строк
-         */
+        int preWin;
+         //Проверка строк
+        preWin = 0;
         for (int x = 0; x < SIZE; x++) {
             for (int y = 0; y < SIZE; y++) {
                 if (map[x][y] == symb) {
                     preWin++;
                     if (preWin == DOTS_TO_WIN) return true;
-                    if (preWin == DOTS_PRE_WIN) {
-                        WARNING = true;
-                        aiThinkX(x);
 
-                    }
                 } else {
                     preWin = 0;
                 }
             }
         }
-        /*
-            Проверка столбцов
-         */
+        //Проверка столбцов
         preWin = 0;
         for (int y = 0; y < SIZE; y++) {
             for (int x = 0; x < SIZE; x++) {
                 if (map[x][y] == symb) {
                     preWin++;
                     if (preWin == DOTS_TO_WIN) return true;
-                    if (preWin == DOTS_PRE_WIN) {
-                        //WARNING = true;
-                    }
                 } else {
                     preWin = 0;
                 }
             }
         }
-        
-        
-        /*
-            Проверка диагонали
-         */
+        //Проверка диагонали
         preWin = 0;
         for (int i = 0; i < SIZE; i++) {
             if (map[i][i] == symb) {
                 preWin++;
                 if (preWin == DOTS_TO_WIN) return true;
-                if (preWin == DOTS_PRE_WIN) {
-                    //WARNING = true;
-
-                }
             } else {
                 preWin = 0;
             }
         }
         preWin = 0;
-        for (int i = 1; i < SIZE; i++) {
+        for (int i = 0; i < SIZE; i++) {
             if (map[i][SIZE - (i + 1)] == symb) {
                 preWin++;
                 if (preWin == DOTS_TO_WIN) return true;
-                if (preWin == DOTS_PRE_WIN) {
-                    //WARNING = true;
-
-                }
             } else {
                 preWin = 0;
             }
         }
 
-        return false;
 
 //        if(map[0][0] == symb && map[0][1] == symb && map[0][2] == symb) return true;
 //        if(map[1][0] == symb && map[1][1] == symb && map[1][2] == symb) return true;
@@ -134,7 +94,7 @@ public class lesson4 {
 //        if (map[0][2] == symb && map[1][2] == symb && map[2][2] == symb) return true;
 //        if (map[0][0] == symb && map[1][1] == symb && map[2][2] == symb) return true;
 //        if (map[2][0] == symb && map[1][1] == symb && map[0][2] == symb) return true;
-//
+        return false;
     }
     public static boolean isMapFull() {
         for (int i = 0; i < SIZE; i++) {
@@ -150,10 +110,6 @@ public class lesson4 {
             x = rand.nextInt(SIZE);
             y = rand.nextInt(SIZE);
         } while (!isCellValid(x, y));
-        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
-        map[y][x] = DOT_O;
-    }
-    public static void aiThinkTurn(int x, int y) {
         System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
         map[y][x] = DOT_O;
     }
@@ -194,4 +150,3 @@ public class lesson4 {
         System.out.println();
     }
 }
-
